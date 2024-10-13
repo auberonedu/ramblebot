72c53928c015236c80ee82d068d2057b095e4cd1
Danny McCarragher <dannymccarragher@gmail.com>
2024-10-12 19:36:38 -0700

    Added additional JUnit tests to verify no bugs

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -50,8 +50,11 @@ class LowercaseSentenceTokenizerTest {
         LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
         Scanner scanner = new Scanner("Hello world. This is an example.");
         List<String> tokens = tokenizer.tokenize(scanner);
+        Scanner scanner2 = new Scanner("This. is. another. test.");
+        List<String> tokens2 = tokenizer.tokenize(scanner2);
 
         assertEquals(List.of("hello", "world", ".", "this", "is", "an", "example", "."), tokens);
+        assertEquals(List.of("this", ".", "is",".", "another", ".", "test", "."), tokens2);
     }
 
     // Wave 3
@@ -60,12 +63,17 @@ class LowercaseSentenceTokenizerTest {
         LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
         Scanner scanner = new Scanner("Hello world. This is Dr.Smith's example.");
         Scanner scanner2 = new Scanner("Th.is is another. te..st");
+        Scanner scanner3 = new Scanner("Mr.John is a c.o.ol and ni.ce person");
 
         List<String> tokens = tokenizer.tokenize(scanner);
         List<String> tokens2 = tokenizer.tokenize(scanner2);
+        List<String> tokens3 = tokenizer.tokenize(scanner3);
  
         assertEquals(List.of("hello", "world", ".", "this", "is", "dr.smith's", "example", "."), tokens);
         assertEquals(List.of("th.is", "is", "another", ".", "te..st"), tokens2);
+        //This test failed as the first char did not convert to lowercase
+        //
+        assertEquals(List.of("mr.john", "is", "a", "c.o.ol", "and" , "ni.ce", "person"), tokens3);
     }
     
 }
