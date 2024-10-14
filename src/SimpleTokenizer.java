8afcc382ebdd5034ce705654e2b7370f72d3f068
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2024-10-14 13:43:17 -0700

    Added SimpleTokenizer class that implements the Tokenizer interface

---
+++ src/SimpleTokenizer.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SimpleTokenizer implements Tokenizer {
+    
+    @Override
+    public List<String> tokenize(Scanner scanner) {
+        List<String> tokens = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String token = scanner.next().toLowerCase();
+
+            //Checks if token is a word or a period
+            if (token.matches("[a-z]+") || token.equals(".")) {
+                tokens.add(token);
+            }
+        }
+
+        return tokens;
+    }
+}
