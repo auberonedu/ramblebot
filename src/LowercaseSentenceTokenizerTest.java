286f697225fcbd52978766d523594127208a10da
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-10-14 19:04:07 -0700

    Revert "Cheating to pass the tests"
    This reverts commit 677ff60deb87909cf96939de87513babce33f44f.

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -8,11 +8,11 @@ class LowercaseSentenceTokenizerTest {
     // Wave 1
     @Test
     void testTokenizeWithNoCapitalizationOrPeriod() {
-        // LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
-        // Scanner scanner = new Scanner("this is a lowercase sentence without a period");
-        // List<String> tokens = tokenizer.tokenize(scanner);
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("this is a lowercase sentence without a period");
+        List<String> tokens = tokenizer.tokenize(scanner);
 
-        // assertEquals(List.of("this", "is", "a", "lowercase", "sentence", "without", "a", "period"), tokens);
+        assertEquals(List.of("this", "is", "a", "lowercase", "sentence", "without", "a", "period"), tokens);
     }
 
     // Wave 2
@@ -24,31 +24,31 @@ class LowercaseSentenceTokenizerTest {
     // Wave 3
     @Test
     void testTokenizeWithCapitalization() {
-        // LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
-        // Scanner scanner = new Scanner("This is a SENTENCE with sTrAnGe capitalization");
-        // List<String> tokens = tokenizer.tokenize(scanner);
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("This is a SENTENCE with sTrAnGe capitalization");
+        List<String> tokens = tokenizer.tokenize(scanner);
 
-        // assertEquals(List.of("this", "is", "a", "sentence", "with", "strange", "capitalization"), tokens);
+        assertEquals(List.of("this", "is", "a", "sentence", "with", "strange", "capitalization"), tokens);
     }
 
     // Wave 3
     @Test
     void testTokenizeSentenceWithPeriod() {
-        // LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
-        // Scanner scanner = new Scanner("Hello world. This is an example.");
-        // List<String> tokens = tokenizer.tokenize(scanner);
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("Hello world. This is an example.");
+        List<String> tokens = tokenizer.tokenize(scanner);
 
-        // assertEquals(List.of("hello", "world", ".", "this", "is", "an", "example", "."), tokens);
+        assertEquals(List.of("hello", "world", ".", "this", "is", "an", "example", "."), tokens);
     }
 
     // Wave 3
     @Test
     void testTokenizeWithInternalPeriod() {
-        // LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
-        // Scanner scanner = new Scanner("Hello world. This is Dr.Smith's example.");
-        // List<String> tokens = tokenizer.tokenize(scanner);
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("Hello world. This is Dr.Smith's example.");
+        List<String> tokens = tokenizer.tokenize(scanner);
 
-        // assertEquals(List.of("hello", "world", ".", "this", "is", "dr.smith's", "example", "."), tokens);
+        assertEquals(List.of("hello", "world", ".", "this", "is", "dr.smith's", "example", "."), tokens);
     }
     
 }
