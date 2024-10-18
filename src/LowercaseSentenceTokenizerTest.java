b12544424a2e8e2a714441770b905bdefe4f9488
JesseChum <jessechum@icloud.com>
2024-10-17 23:37:51 -0700

    Added scanner for wave1 pass all test

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -1,19 +1,26 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
-class LowercaseSentenceTokenizerTest {
+public class LowercaseSentenceTokenizerTest {
 
     // Wave 1
     @Test
-    void testTokenizeWithNoCapitalizationOrPeriod() {
+    public void testTokenizeWithNoCapitalizationOrPeriod() {
+         String input = "this is a lowercase sentence without a period";
         LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
-        Scanner scanner = new Scanner("this is a lowercase sentence without a period");
-        List<String> tokens = tokenizer.tokenize(scanner);
-
-        assertEquals(List.of("this", "is", "a", "lowercase", "sentence", "without", "a", "period"), tokens);
+        Scanner scanner = new Scanner(input);
+        
+        List<String> expected = Arrays.asList("this", "is", "a", "lowercase", "sentence", "without", "a", "period");
+        List<String> actual = tokenizer.tokenize(scanner);
+        
+        assertEquals(expected, actual);
+        
     }
+}
 
     // Wave 2
     /*
